8c58c4d947e0444f3e214c46a7931855f72eb079
ShadowLeaderJordanzo <boberjones123@hotmail.com>
2022-07-19 16:53:19 -0400

    hm
    testing stuff

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-def checkName(name): 
-    answer = input("Is your name " + name + "? ")
+from tkinter import *
+import os
+os.system('clear')
 
-    if answer.lower() == "yes":
-        print("Hello,", name)
-    else:
-        print("we're sorry about that, m'lady")
 
-checkName("Bober")
+
+root = Tk()
+root.title('Sigrogana Legend 2 Calculator')
+root.geometry("400x600")
+
+root.mainloop()
+
